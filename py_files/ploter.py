adf9536b28fdb4928a9d26fd164ff0817fca052e
BeGIMATH <begatimi_95@outlook.com>
2019-12-02 18:03:25 +0100

    Modified the ploter so that it shows tooltips!

--- py_files/ploter.py
+++ py_files/ploter.py
@@ -3,12 +3,13 @@ sys.path.append("../build/lib")
 
 
 import tkinter as tk
+
 import tp4
 from tkinter import messagebox as mBox
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from math import sqrt
-
+import tkinter.tix as tix
 
 
 from matplotlib.patches import FancyArrowPatch
@@ -24,6 +25,10 @@ from matplotlib.figure import Figure
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+
+
+
+
 class Arrow3D(FancyArrowPatch):
     def __init__(self, xs, ys, zs, *args, **kwargs):
         FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
@@ -35,7 +40,43 @@ class Arrow3D(FancyArrowPatch):
         self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
         FancyArrowPatch.draw(self, renderer)
 
-
+''' tk_ToolTip_class101.py
+gives a Tkinter widget a tooltip as the mouse is above the widget
+tested with Python27 and Python34  by  vegaseat  09sep2014
+'''
+try:
+    # for Python2
+    import Tkinter as tk
+except ImportError:
+    # for Python3
+    import tkinter as tk
+class CreateToolTip(object):
+    '''
+    create a tooltip for a given widget
+    '''
+    def __init__(self, widget, text='widget info'):
+        self.widget = widget
+        self.text = text
+        self.widget.bind("<Enter>", self.enter)
+        self.widget.bind("<Leave>", self.close)
+    def enter(self, event=None):
+        x = y = 0
+        x, y, cx, cy = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() + 25
+        y += self.widget.winfo_rooty() + 20
+        # creates a toplevel window
+        self.tw = tk.Toplevel(self.widget)
+        # Leaves only the label and removes the app window
+        self.tw.wm_overrideredirect(True)
+        self.tw.wm_geometry("+%d+%d" % (x, y))
+        label = tk.Label(self.tw, text=self.text, justify='left',
+                       background='yellow', relief='solid', borderwidth=1,
+                       font=("times", "8", "normal"))
+        label.pack(ipadx=1)
+    def close(self, event=None):
+        if self.tw:
+            self.tw.destroy()
+#
 # creating basic window
 window = tk.Tk()
 #window.geometry("500x500") # size of the window width:- 500, height:- 375
@@ -43,7 +84,8 @@ window = tk.Tk()
 window.title("3D Vector Ploter")
 
 def _msgBox():
-    mBox.showinfo('Thanks for installing this software!', ' A simple 3DPloter of Vectors in')
+    mBox.showinfo('Thanks for installing this software!', ' A simple 3DPloter of Vectors using Matplotlib!')
+
 
 
 
@@ -58,55 +100,74 @@ file_menu.add_command(label="Exit", command = window.quit)
 
 about_menu = tk.Menu(root_menu)
 root_menu.add_cascade(label="About",menu=about_menu)
-about_menu.add_command(label="About the Calculator",command=_msgBox)
+about_menu.add_command(label="About the Ploter!",command=_msgBox)
 
 
 
 
-win1 = tk.LabelFrame(window)
+win1 = tk.LabelFrame(window,bg="lightBlue2")
 win1.grid(column=0,row=0, sticky="wesn")
 
 x_1 = tk.DoubleVar()
 x_1.set("")
 
-tk.Label(win1,text=" v1 ").grid(row=0,column=0,padx=10,pady=10)
-tk.Entry(win1,textvariable=x_1,width=3).grid(row=1,column=0,padx=10,pady=10)
+tk.Label(win1,text=" v1 ",bg="sky blue").grid(row=0,column=0,padx=10,pady=10)
+
+v11 = tk.Entry(win1,textvariable=x_1,width=3)
+v11.grid(row=1,column=0,padx=10,pady=10)
+CreateToolTip(v11,"Enter the first coordinate!")
 
 
 x_2 = tk.DoubleVar()
 x_2.set("")
 
-tk.Entry(win1,textvariable=x_2,width=3).grid(row=2,column=0,padx=10,pady=10)
+v12 = tk.Entry(win1,textvariable=x_2,width=3)
+v12.grid(row=2,column=0,padx=10,pady=10)
+CreateToolTip(v12,"Enter the second coordinate!")
+
+
 
 x_3 = tk.DoubleVar()
 x_3.set("")
 
-tk.Entry(win1,textvariable=x_3,width=3).grid(row=3,column=0,padx=10,pady=10)
+v13 = tk.Entry(win1,textvariable=x_3,width=3)
+v13.grid(row=3,column=0,padx=10,pady=10)
+CreateToolTip(v13,"Enter the third coordinate!")
+
 
 
 X_1 = tk.DoubleVar()
 X_1.set("")
-tk.Label(win1,text=" v2 ").grid(row=0,column=1,padx=10,pady=10)
+tk.Label(win1,text=" v2 ",bg="sky blue").grid(row=0,column=1,padx=10,pady=10)
 
-tk.Entry(win1,textvariable=X_1,width=3).grid(row=1,column=1,padx=10,pady=10)
+v21 = tk.Entry(win1,textvariable=X_1,width=3)
+v21.grid(row=1,column=1,padx=10,pady=10)
+CreateToolTip(v21,"Enter the first coordinate!")
+
 
 X_2 = tk.DoubleVar()
 X_2.set("")
 
 
-tk.Entry(win1,textvariable=X_2,width=3).grid(row=2,column=1,padx=10,pady=10)
+v22 = tk.Entry(win1,textvariable=X_2,width=3)
+v22.grid(row=2,column=1,padx=10,pady=10)
+CreateToolTip(v22,"Enter the second coordinate!")
+
 
 
 X_3 = tk.DoubleVar()
 X_3.set("")
 
-tk.Entry(win1,textvariable=X_3,width=3).grid(row=3,column=1,padx=10,pady=10)
+v23 = tk.Entry(win1,textvariable=X_3,width=3)
+v23.grid(row=3,column=1,padx=10,pady=10)
+CreateToolTip(v23,"Enter the third coordinate!")
+
 
 
 Xx_1 = tk.DoubleVar()
 Xx_1.set("")
 
-tk.Label(win1,text="v1 + v2").grid(row=0,column=2,padx=10,pady=10)
+tk.Label(win1,text="v1 + v2",bg="sky blue").grid(row=0,column=2,padx=10,pady=10)
 tk.Entry(win1,textvariable=Xx_1,width=4,state="readonly").grid(row=1,column=2,padx=10,pady=10)
 
 Xx_2 = tk.DoubleVar()
@@ -125,7 +186,7 @@ tk.Entry(win1,textvariable=Xx_3,width=4,state="readonly").grid(row=3,column=2,pa
 XX_1 = tk.DoubleVar()
 XX_1.set("")
 
-tk.Label(win1,text="v1 x v2").grid(row=0,column=3,padx=10,pady=10)
+tk.Label(win1,text="v1 x v2",bg="sky blue").grid(row=0,column=3,padx=10,pady=10)
 tk.Entry(win1,textvariable=XX_1,width=4,state="readonly").grid(row=1,column=3,padx=10,pady=10)
 
 XX_2 = tk.DoubleVar()
@@ -142,7 +203,7 @@ tk.Entry(win1,textvariable=XX_3,width=4,state="readonly").grid(row=3,column=3,pa
 
 Dot = tk.DoubleVar()
 Dot.set(0)
-tk.Label(win1,text="<v1,v2>").grid(row=0,column=6,padx=10,pady=10)
+tk.Label(win1,text="<v1,v2>",bg="sky blue").grid(row=0,column=6,padx=10,pady=10)
 
 tk.Entry(win1,textvariable=Dot,width=3,state="readonly").grid(row=1,column=6,padx=10,pady=10)
 
@@ -186,10 +247,20 @@ def angle():
 
 
 
-tk.Button(win1, text='<.> ', command=dot).grid(column=5,row=3,padx=10,pady=10)
-tk.Button(win1, text=' x  ', command=cross_prod).grid(column=5,row=2,padx=10,pady=10)
-tk.Button(win1, text=' +  ', command=add_vectors).grid(column=5,row=1,padx=10,pady=10)
-tk.Button(win1, text= '\u03F4(,)',command=angle).grid(column=6,row=2,padx=10,pady=10)
+scal=tk.Button(win1, text='<.> ', command=dot,bg="deep sky blue")
+scal.grid(column=5,row=3,padx=10,pady=10)
+CreateToolTip(scal,"Compute the scalar product!")
+
+vec =tk.Button(win1, text=' x  ', command=cross_prod,bg="deep sky blue")
+vec.grid(column=5,row=2,padx=10,pady=10)
+CreateToolTip(vec,"Compute the vectorial product!")
+
+add = tk.Button(win1, text=' +  ', command=add_vectors,bg="deep sky blue")
+add.grid(column=5,row=1,padx=10,pady=10)
+CreateToolTip(add,"Add vectors, coordinate by coordinate!")
+ang = tk.Button(win1, text= '\u03F4(,)',command=angle,bg="deep sky blue")
+ang.grid(column=6,row=2,padx=10,pady=10)
+CreateToolTip(ang,"Compute the angle between two vectors!")
 #tk.Button(win1,text="Rotate",command=rotate).grid(column=5,row=1)
 
 win2 = tk.LabelFrame(window)
@@ -199,7 +270,10 @@ win2.grid(columnspan=2,row=1, sticky="nsew")
 
 def plot_vectors():
 
-   
+    x = np.linspace(-10,10,100)
+    y = np.linspace(-10,10,100)
+    X,Y = np.meshgrid(x,y)
+    Z = 0*X*Y
    
     # define origin
     o = np.array([0,0,0])
@@ -222,7 +296,7 @@ def plot_vectors():
     canvas = FigureCanvasTkAgg(fig,win2)
         
     ax = fig.add_subplot(111, projection='3d')
-   
+    
     # plot ov1v2z0 axes
     a = Arrow3D([o[0], v1[0]], [o[1], v1[1]], [o[2], v1[2]], mutation_scale=20, arrowstyle='-|>', color='k')
     ax.add_artist(a)
@@ -258,9 +332,9 @@ def plot_vectors():
     #ax.set_xlim(-10,10)
     # show figure
     ax.view_init(elev=-150, azim=0)
-
+    #ax.contour3D(X,Y,Z)
     
-    ax.set_axis_off()
+    #ax.set_axis_off()
     
     plt.show()
 
@@ -272,5 +346,7 @@ def plot_vectors():
     canvas.get_tk_widget().grid(row=0,column=0)
     print(v1)
 
-tk.Button(win1,text="Plot",command=plot_vectors).grid(column=5,row=0)
+pllot = tk.Button(win1,text="Plot",command=plot_vectors,bg="deep sky blue")
+pllot.grid(column=5,row=0)
+CreateToolTip(pllot,"Plot the vectors in 3d!")
 window.mainloop()
